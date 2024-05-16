7b4bfc556bb5634f346b4b97a9312e5fa95c721c
hunter <hunterjsb@gmail.com>
2024-05-15 21:58:04 -0400

    change to localhost

--- internal/server/utils.go
+++ internal/server/utils.go
@@ -4,7 +4,6 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"log"
-	"time"
 )
 
 func RandLink(length int) string {
@@ -15,19 +14,10 @@ func RandLink(length int) string {
 		log.Fatal(err)
 	}
 
-	// Add additional entropy using the current timestamp
-	timestamp := time.Now().UnixNano()
-	timestampBytes := make([]byte, 8)
-	for i := 0; i < 8; i++ {
-		timestampBytes[i] = byte(timestamp >> (i * 8))
-	}
-	b = append(b, timestampBytes...)
-
-	// Encode the byte slice using base64 RawURLEncoding
-	randomString := base64.RawURLEncoding.EncodeToString(b)
+	randomString := base64.URLEncoding.EncodeToString(b)
 
 	// Generate the secure link URL
-	link := "https://tritium.example.com/verify/" + randomString
+	link := "http://localhost:8080/verify/" + randomString
 
 	return link
 }
